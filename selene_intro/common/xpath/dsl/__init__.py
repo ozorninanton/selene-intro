4445246b002cb23b27c630260748cefe1deff40f
ozornoy <azornin92@gmail.com>
2024-05-11 17:17:42 +0500

    task4_plus(4)

--- selene_intro/common/xpath/dsl/__init__.py
+++ selene_intro/common/xpath/dsl/__init__.py
@@ -1,5 +1,5 @@
 from selene_intro.common.xpath.dsl import selector
-from selene_intro.common.xpath.dsl.predicate import PredicateProxy
+from selene_intro.common.xpath.dsl.predicate import Predicate
 
-its = PredicateProxy()
+its = Predicate()
 x = selector
--- selene_intro/common/xpath/dsl/predicate.py
+++ selene_intro/common/xpath/dsl/predicate.py
@@ -1,26 +1,36 @@
 class Predicate:
-    def __init__(self, as_not=False):
-        self.__as_not = as_not
+    def __init__(self, expression=None, not_=False):
+        self.expression = expression
+        self.__not = not_
+
+    def __str__(self):
+        return self.expression
 
     def id(self, value):
-        return self.__apply_not_(f'@id="{value}"')
+        return Predicate(self.__apply_not_(f'@id="{value}"'))
 
     def descendant_with_text(self, value):
-        return self.__apply_not_(f'.//text()="{value}"')
+        return Predicate(self.__apply_not_(f'.//text()="{value}"'))
 
     def css_class(self, value):
-        return self.__apply_not_(
-            f'contains(concat(" ", normalize-space(@class), " "), " {value} ")'
+        return Predicate(
+            self.__apply_not_(
+                f'contains(concat(" ", normalize-space(@class), " "), " {value} ")'
+            )
         )
 
     def __apply_not_(self, expression):
-        if self.__as_not:
-            return f"not({expression})"
+        if self.__not:
+            return f'not({expression})'
         else:
             return expression
 
+    def and_(self, predicate):
+        return Predicate(f'{self.expression} and {predicate}')
 
-class PredicateProxy(Predicate):
+    def or_(self, predicate):
+        return Predicate(f'{self.expression} or {predicate}')
+
     @property
     def not_(self):
-        return Predicate(as_not=True)
+        return Predicate(not_=True)
