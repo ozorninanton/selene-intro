6a91deb07aec89210eecf8c24fa98c02ee1cfaf2
ozornoy <azornin92@gmail.com>
2024-05-25 21:32:05 +0500

    task4_plus(5)

--- selene_intro/common/xpath/dsl/predicate.py
+++ selene_intro/common/xpath/dsl/predicate.py
@@ -1,35 +1,34 @@
 class Predicate:
-    def __init__(self, expression=None, not_=False):
-        self.expression = expression
+    def __init__(self, condition=None, not_=False):
+        self.__condition = condition
         self.__not = not_
 
     def __str__(self):
-        return self.expression
+        condition = f'not({self.__condition})' if self.__not else self.__condition
+        self.__condition, self.__not = None, False
+        return condition
 
     def id(self, value):
-        return Predicate(self.__apply_not_(f'@id="{value}"'))
+        return Predicate(f'@id="{value}"', self.__not)
 
     def descendant_with_text(self, value):
-        return Predicate(self.__apply_not_(f'.//text()="{value}"'))
+        return Predicate(f'.//text()="{value}"', self.__not)
 
     def css_class(self, value):
         return Predicate(
-            self.__apply_not_(
-                f'contains(concat(" ", normalize-space(@class), " "), " {value} ")'
-            )
+            f'contains(concat(" ", normalize-space(@class), " "), " {value} ")',
+            self.__not
         )
 
-    def __apply_not_(self, expression):
-        if self.__not:
-            return f'not({expression})'
-        else:
-            return expression
-
     def and_(self, predicate):
-        return Predicate(f'{self.expression} and {predicate}')
+        if self.__condition is None:
+            raise Exception('cannot be called while __condition is None')
+        return Predicate(f'{self.__condition} and {predicate}')
 
     def or_(self, predicate):
-        return Predicate(f'{self.expression} or {predicate}')
+        if self.__condition is None:
+            raise Exception('cannot be called while __condition is None')
+        return Predicate(f'{self.__condition} or {predicate}')
 
     @property
     def not_(self):
